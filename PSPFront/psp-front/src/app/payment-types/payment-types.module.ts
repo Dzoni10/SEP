ededc73fc7137b745391af8e05c7abfb5175997c
Dzoni10 <nikola.milivojevic.555@gmail.com>
2026-02-03 09:15:31 +0100

    initial bank

--- PSPFront/psp-front/src/app/payment-types/payment-types.module.ts
+++ PSPFront/psp-front/src/app/payment-types/payment-types.module.ts
@@ -10,6 +10,7 @@ import {MatIconModule} from '@angular/material/icon'
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { MatButtonModule } from '@angular/material/button';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
@@ -25,7 +26,8 @@ import { MatButtonModule } from '@angular/material/button';
     MatIconModule,
     MatFormFieldModule,
     MatInputModule,
-    MatButtonModule
+    MatButtonModule,
+    BrowserAnimationsModule
   ]
 })
 export class PaymentTypesModule { }
